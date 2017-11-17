18cab2d9b32c431110c59ce0c78ed1dc079746fb
Dan Pidcock <dan@Dans-MacBook.local>
2017-11-17 18:20:18 +0000

    Test model division

--- src/test/scala/ModelTest.scala
+++ src/test/scala/ModelTest.scala
@@ -1,4 +1,5 @@
 import org.scalatest._
+import collection.mutable.HashMap
 import svlis.lib._
 import svlis.utils._
 
@@ -11,11 +12,20 @@ class ModelSpec extends FlatSpec with Matchers {
 
     val box = new Box(new Point(0, 0, 0), new Point(2, 2, 2))
     val model = Model(set, box)
+  }
+
+  it should "divide into solid and air" in {
+    val normal = new Point(-1, 1, 0)
+    val through = new Point(1, 1, 1)
+    val set = new Set(new Primitive(new Plane(normal, through)))
+
+    val box = new Box(new Point(0, 0, 0), new Point(2, 2, 2))
+    val model = Model(set, box)
 
     def mid(iv: Interval): Double = iv.lo + (iv.hi - iv.lo) / 2.0d
 
     def decision(m: Model, level: Int): DivideDecision = {
-      if (level > 13 || m.set.contents <= Contents.Nothing) new DivideDecision(ModelKind.Leaf, 0)
+      if (level > 4 || m.set.contents <= Contents.Nothing) new DivideDecision(ModelKind.Leaf, 0)
       else level % 3 match {
         case 0 => new DivideDecision(ModelKind.XDiv, mid(m.box.xi))
         case 1 => new DivideDecision(ModelKind.YDiv, mid(m.box.yi))
@@ -24,8 +34,19 @@ class ModelSpec extends FlatSpec with Matchers {
     }
 
     val divided = model.divide(decision)
-    // TODO test divided boxes contents
-    // TODO test some of air, solid, surface
+
+    val boxContents = new HashMap[Int, Int]()
+    def checkBox(m: Model, level: Int) = {
+      // the plane is through the line x = y so should divided as air/solid on y > x
+      if (m.box.yi.lo > m.box.xi.hi) m.set.contents should be(Contents.Nothing)
+      if (m.box.yi.hi < m.box.xi.lo) m.set.contents should be(Contents.Everything)
+      boxContents(m.set.contents) = boxContents.getOrElseUpdate(m.set.contents, 0) + 1
+    }
+    divided.walk(checkBox)
+    assert(boxContents(Contents.Nothing) > 0)
+    assert(boxContents(Contents.Everything) > 0)
+    assert(boxContents(1) > 0)
+
     ModelDump.dump(divided)
     ModelToVrml.write(divided, "divided.wrl", true)
   }
